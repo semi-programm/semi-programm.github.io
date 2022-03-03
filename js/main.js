9f4f880714f20dc2e78d77f67af30c9e58ebebc7
utsugi_m <utsugi.mitsunori@gmail.com>
2022-03-04 02:45:58 +0900

    Update ちょっと修正WIP

--- js/main.js
+++ js/main.js
@@ -79,39 +79,12 @@
     };
 
 
-
-    var goToTop = function () {
-
-        $('.js-gotop').on('click', function (event) {
-
-            event.preventDefault();
-
-            $('html, body').animate({
-                scrollTop: $('html').offset().top
-            }, 500, 'easeInOutExpo');
-
-            return false;
-        });
-
-        $(window).scroll(function () {
-
-            var $win = $(window);
-            if ($win.scrollTop() > 200) {
-                $('.js-top').addClass('active');
-            } else {
-                $('.js-top').removeClass('active');
-            }
-
-        });
-
-    };
-
     var pieChart = function () {
         $('.chart').easyPieChart({
             scaleColor: false,
             lineWidth: 4,
             lineCap: 'butt',
-            barColor: '#FF9000',
+            barColor: 'green',
             trackColor: "#f5f5f5",
             size: 160,
             animate: 1000
@@ -142,7 +115,6 @@
 
     $(function () {
         contentWayPoint();
-        goToTop();
         loaderPage();
         fullHeight();
         parallax();
